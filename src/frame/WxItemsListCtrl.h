03116bbfba254540a29df08a80906a5c265c4f06
zz <zhuguang@ejoy.com>
2018-04-20 08:01:06 +0800

    [ADDED] items list

---
+++ src/frame/WxItemsListCtrl.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <ee0/Observer.h>
+
+#include <wx/treectrl.h>
+
+namespace eone
+{
+
+class WxItemsListCtrl : public wxTreeCtrl, public ee0::Observer
+{
+public:
+
+
+}; // WxItemsListCtrl
+
+}
