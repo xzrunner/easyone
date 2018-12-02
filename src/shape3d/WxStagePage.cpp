b5472ce7a68a45a2dd64a83782f6ae685d50d3ea
zz <zhuguang@ejoy.com>
2018-12-02 21:04:48 +0800

    [CHANGED] shape3d stage panel

--- src/shape3d/WxStagePage.cpp
+++ src/shape3d/WxStagePage.cpp
@@ -30,7 +30,7 @@ namespace shape3d
 const std::string WxStagePage::PAGE_TYPE = "shape3d";
 
 WxStagePage::WxStagePage(wxWindow* parent, ee0::WxLibraryPanel* library, ECS_WORLD_PARAM const ee0::GameObj& obj)
-	: eone::WxStagePage(parent, ECS_WORLD_VAR obj, SHOW_LIBRARY | SHOW_RECORD | SHOW_STAGE | SHOW_STAGE_EXT | SHOW_WORLD | SHOW_DETAIL | SHOW_SCRIPT)
+	: eone::WxStagePage(parent, ECS_WORLD_VAR obj, SHOW_STAGE)
 	, m_editor_mgr(*this)
 	, m_cam_mgr(false)
 {
