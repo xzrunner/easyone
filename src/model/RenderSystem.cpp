e90b5c2fbf591bc84e602bb6626ae6f3be65ef8f
zz <xzrunner@gmail.com>
2019-01-08 17:39:19 +0800

    [CHANGED] light position

--- src/model/RenderSystem.cpp
+++ src/model/RenderSystem.cpp
@@ -98,7 +98,7 @@ void RenderSystem::DrawSkeletalNode(const ::model::ModelInstance& model_inst,
 				mgr->SetBoneMatrixes(effect_type, &mat, 1);
 			}
 
-			mgr->SetLightPosition(effect_type, sm::vec3(0.25f, 0.25f, 1));
+			mgr->SetLightPosition(effect_type, sm::vec3(0, 2, -4));
 			mgr->SetProjMat(effect_type, pt3::Blackboard::Instance()->GetWindowContext()->GetProjMat().x);
 			mgr->SetNormalMat(effect_type, child_mat);
 
