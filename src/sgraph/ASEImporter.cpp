f9ce3b917c2a338ba1800560ddb5d0e62f70a22f
zz <zhuguang@ejoy.com>
2019-03-18 07:26:04 +0800

    [CHANGED] add custom node, var check to query

--- src/sgraph/ASEImporter.cpp
+++ src/sgraph/ASEImporter.cpp
@@ -13,7 +13,10 @@
 #include <shadergraph/RegistNodes.h>
 #include <shadergraph/PinsType.h>
 #include <shadergraph/ReflectPropTypes.h>
+#include <shadergraph/node/Custom.h>
+#include <shadergraph/node/StandardSurfaceOutput.h>
 
+#include <cpputil/StringHelper.h>
 #include <node0/SceneNode.h>
 #include <node0/CompIdentity.h>
 #include <node2/CompTransform.h>
@@ -21,73 +24,93 @@
 #include <aseimp/FileLoader.h>
 #include <aseimp/typedef.h>
 
+#include <boost/filesystem.hpp>
+
 namespace
 {
 
 bool CheckBool(const aseimp::FileLoader::Node& node, const std::string& name)
 {
     auto itr = node.vars.find(name);
-    assert(itr != node.vars.end() && itr->second.type == aseimp::VarType::Bool);
+    if (itr == node.vars.end() || itr->second.type != aseimp::VarType::Bool) {
+        throw std::exception("check fail.");
+    }
     return itr->second.b;
 }
 
 int CheckInt(const aseimp::FileLoader::Node& node, const std::string& name)
 {
     auto itr = node.vars.find(name);
-    assert(itr != node.vars.end() && itr->second.type == aseimp::VarType::Int);
+    if (itr == node.vars.end() || itr->second.type != aseimp::VarType::Int) {
+        throw std::exception("check fail.");
+    }
     return itr->second.i;
 }
 
 float CheckFloat(const aseimp::FileLoader::Node& node, const std::string& name)
 {
     auto itr = node.vars.find(name);
-    assert(itr != node.vars.end() && itr->second.type == aseimp::VarType::Float);
+    if (itr == node.vars.end() || itr->second.type != aseimp::VarType::Float) {
+        throw std::exception("check fail.");
+    }
     return itr->second.f;
 }
 
 const char* CheckString(const aseimp::FileLoader::Node& node, const std::string& name)
 {
     auto itr = node.vars.find(name);
-    assert(itr != node.vars.end() && itr->second.type == aseimp::VarType::String);
+    if (itr == node.vars.end() || itr->second.type != aseimp::VarType::String) {
+        throw std::exception("check fail.");
+    }
     return itr->second.c;
 }
 
-int port_type_aseimp_to_bp(aseimp::WirePortDataType type)
+bool QueryBool(const aseimp::FileLoader::Node& node, const std::string& name, bool& ret)
 {
-    int ret = bp::PINS_ANY_VAR;
-    switch (type)
-    {
-    case aseimp::WirePortDataType::FLOAT:
-        ret = sg::PINS_VECTOR1;
-        break;
-    case aseimp::WirePortDataType::FLOAT2:
-        ret = sg::PINS_VECTOR2;
-        break;
-    case aseimp::WirePortDataType::FLOAT3:
-        ret = sg::PINS_VECTOR3;
-        break;
-    case aseimp::WirePortDataType::FLOAT4:
-        ret = sg::PINS_VECTOR4;
-        break;
-    case aseimp::WirePortDataType::COLOR:
-        ret = sg::PINS_COLOR;
-        break;
-    case aseimp::WirePortDataType::SAMPLER2D:
-        ret = sg::PINS_TEXTURE2D;
-        break;
-    case aseimp::WirePortDataType::FLOAT3x3:
-        ret = sg::PINS_MATRIX3;
-        break;
-    case aseimp::WirePortDataType::FLOAT4x4:
-        ret = sg::PINS_MATRIX4;
-        break;
-    case aseimp::WirePortDataType::OBJECT:
-        ret = bp::PINS_ANY_VAR;
-        break;
-    default:
-        assert(0);
+    auto itr = node.vars.find(name);
+    if (itr == node.vars.end()) {
+        return false;
     }
-    return ret;
+
+    assert(itr->second.type == aseimp::VarType::Bool);
+    ret = itr->second.b;
+    return true;
+}
+
+bool QueryInt(const aseimp::FileLoader::Node& node, const std::string& name, int& ret)
+{
+    auto itr = node.vars.find(name);
+    if (itr == node.vars.end()) {
+        return false;
+    }
+
+    assert(itr->second.type == aseimp::VarType::Int);
+    ret = itr->second.i;
+    return true;
+}
+
+bool QueryFloat(const aseimp::FileLoader::Node& node, const std::string& name, float& ret)
+{
+    auto itr = node.vars.find(name);
+    if (itr == node.vars.end()) {
+        return false;
+    }
+
+    assert(itr->second.type == aseimp::VarType::Float);
+    ret = itr->second.f;
+    return true;
+}
+
+bool QueryString(const aseimp::FileLoader::Node& node, const std::string& name, std::string& ret)
+{
+    auto itr = node.vars.find(name);
+    if (itr == node.vars.end()) {
+        return false;
+    }
+
+    assert(itr->second.type == aseimp::VarType::String);
+    ret = itr->second.c;
+    return true;
 }
 
 }
@@ -106,17 +129,21 @@ void ASEImporter::LoadAsset(const std::string& filepath)
 {
     aseimp::FileLoader loader;
     loader.LoadAsset(filepath);
-    Load(loader);
+
+    auto dir = boost::filesystem::path(filepath).parent_path().string();
+    Load(loader, dir);
 }
 
 void ASEImporter::LoadShader(const std::string& filepath)
 {
     aseimp::FileLoader loader;
     loader.LoadShader(filepath);
-    Load(loader);
+
+    auto dir = boost::filesystem::path(filepath).parent_path().string();
+    Load(loader, dir);
 }
 
-void ASEImporter::Load(const aseimp::FileLoader& loader)
+void ASEImporter::Load(const aseimp::FileLoader& loader, const std::string& dir)
 {
     for (auto& src : loader.GetNodes())
     {
@@ -146,10 +173,13 @@ void ASEImporter::Load(const aseimp::FileLoader& loader)
         {
             auto input = std::make_shared<bp::node::Input>();
 
-            input->SetName(CheckString(src, "name"));
+            std::string name;
+            if (QueryString(src, "name", name)) {
+                input->SetName(name);
+            }
 
             assert(src.outputs.size() == 1);
-            input->SetType(port_type_aseimp_to_bp(src.outputs[0].data_type));
+            input->SetType(PortTypeASEImpToBP(src.outputs[0].data_type));
 
             bp_node = input;
         }
@@ -158,10 +188,13 @@ void ASEImporter::Load(const aseimp::FileLoader& loader)
         {
             auto output = std::make_shared<bp::node::Output>();
 
-            output->SetName(CheckString(src, "name"));
+            std::string name;
+            if (QueryString(src, "name", name)) {
+                output->SetName(name);
+            }
 
             assert(src.inputs.size() == 1);
-            output->SetType(port_type_aseimp_to_bp(src.inputs[0].data_type));
+            output->SetType(PortTypeASEImpToBP(src.inputs[0].data_type));
 
             bp_node = output;
         }
@@ -169,44 +202,70 @@ void ASEImporter::Load(const aseimp::FileLoader& loader)
         case aseimp::NodeClass::Float:
         {
             auto vec1 = std::make_shared<sg::node::Vector1>();
-            vec1->SetName(CheckString(src, "name"));
-            vec1->SetValue(CheckFloat(src, "x"));
+
+            std::string name;
+            if (QueryString(src, "name", name)) {
+                vec1->SetName(name);
+            }
+            float x;
+            if (QueryFloat(src, "x", x)) {
+                vec1->SetValue(x);
+            }
+
             bp_node = vec1;
         }
             break;
         case aseimp::NodeClass::Vector2:
         {
             auto vec2 = std::make_shared<sg::node::Vector2>();
-            vec2->SetName(CheckString(src, "name"));
-            vec2->SetValue(sm::vec2(
-                CheckFloat(src, "x"),
-                CheckFloat(src, "y")
-            ));
+
+            std::string name;
+            if (QueryString(src, "name", name)) {
+                vec2->SetName(name);
+            }
+            float x, y;
+            if (QueryFloat(src, "x", x) &&
+                QueryFloat(src, "y", y)) {
+                vec2->SetValue(sm::vec2(x, y));
+            }
+
             bp_node = vec2;
         }
             break;
         case aseimp::NodeClass::Vector3:
         {
             auto vec3 = std::make_shared<sg::node::Vector3>();
-            vec3->SetName(CheckString(src, "name"));
-            vec3->SetValue(sm::vec3(
-                CheckFloat(src, "x"),
-                CheckFloat(src, "y"),
-                CheckFloat(src, "z")
-            ));
+
+            std::string name;
+            if (QueryString(src, "name", name)) {
+                vec3->SetName(name);
+            }
+            float x, y, z;
+            if (QueryFloat(src, "x", x) &&
+                QueryFloat(src, "y", y) &&
+                QueryFloat(src, "z", z)) {
+                vec3->SetValue(sm::vec3(x, y, z));
+            }
+
             bp_node = vec3;
         }
             break;
         case aseimp::NodeClass::Vector4:
         {
             auto vec4 = std::make_shared<sg::node::Vector4>();
-            vec4->SetName(CheckString(src, "name"));
-            vec4->SetValue(sm::vec4(
-                CheckFloat(src, "x"),
-                CheckFloat(src, "y"),
-                CheckFloat(src, "z"),
-                CheckFloat(src, "w")
-            ));
+
+            std::string name;
+            if (QueryString(src, "name", name)) {
+                vec4->SetName(name);
+            }
+            float x, y, z, w;
+            if (QueryFloat(src, "x", x) &&
+                QueryFloat(src, "y", y) &&
+                QueryFloat(src, "z", z) &&
+                QueryFloat(src, "w", w)) {
+                vec4->SetValue(sm::vec4(x, y, z, w));
+            }
+
             bp_node = vec4;
         }
             break;
@@ -219,16 +278,26 @@ void ASEImporter::Load(const aseimp::FileLoader& loader)
         case aseimp::NodeClass::ViewDirection:
         {
             auto view_dir = std::make_shared<sg::node::ViewDirection>();
-            switch (CheckInt(src, "view_dir_space"))
+
+            int view_dir_space;
+            if (QueryInt(src, "view_dir_space", view_dir_space))
             {
-            case aseimp::ViewSpace::Tangent:
-                view_dir->SetViewSpace(sg::ViewSpace::Tangent);
-                break;
-            case aseimp::ViewSpace::World:
-                view_dir->SetViewSpace(sg::ViewSpace::World);
-                break;
+                switch (view_dir_space)
+                {
+                case aseimp::ViewSpace::Tangent:
+                    view_dir->SetViewSpace(sg::ViewSpace::Tangent);
+                    break;
+                case aseimp::ViewSpace::World:
+                    view_dir->SetViewSpace(sg::ViewSpace::World);
+                    break;
+                }
             }
-            view_dir->SetSafeNormalize(CheckBool(src, "safe_normalize"));
+
+            bool safe_normalize;
+            if (QueryBool(src, "safe_normalize", safe_normalize)) {
+                view_dir->SetSafeNormalize(safe_normalize);
+            }
+
             bp_node = view_dir;
         }
             break;
@@ -247,19 +316,29 @@ void ASEImporter::Load(const aseimp::FileLoader& loader)
         case aseimp::NodeClass::ChannelMask:
         {
             auto cm = std::make_shared<sg::node::ChannelMask>();
-            sg::PropMultiChannels c = 0;
-            if (CheckBool(src, "r")) {
-                c.channels |= sg::PropMultiChannels::CHANNEL_R;
+
+            bool r, g, b, a;
+            if (QueryBool(src, "r", r) &&
+                QueryBool(src, "g", g) &&
+                QueryBool(src, "b", b) &&
+                QueryBool(src, "a", a))
+            {
+                sg::PropMultiChannels c = 0;
+                if (r) {
+                    c.channels |= sg::PropMultiChannels::CHANNEL_R;
+                }
+                if (g) {
+                    c.channels |= sg::PropMultiChannels::CHANNEL_G;
+                }
+                if (b) {
+                    c.channels |= sg::PropMultiChannels::CHANNEL_B;
+                }
+                if (a) {
+                    c.channels |= sg::PropMultiChannels::CHANNEL_A;
+                }
+                cm->SetChannels(c);
             }
-            if (CheckBool(src, "g")) {
-                c.channels |= sg::PropMultiChannels::CHANNEL_G;
-            }
-            if (CheckBool(src, "b")) {
-                c.channels |= sg::PropMultiChannels::CHANNEL_B;
-            }
-            if (CheckBool(src, "a")) {
-                c.channels |= sg::PropMultiChannels::CHANNEL_A;
-            }
+
             bp_node = cm;
         }
             break;
@@ -269,9 +348,22 @@ void ASEImporter::Load(const aseimp::FileLoader& loader)
         case aseimp::NodeClass::Commentary:
         {
             auto comm = std::make_shared<bp::node::Commentary>();
-            comm->SetWidth(CheckFloat(src, "width"));
-            comm->SetHeight(CheckFloat(src, "height"));
-            comm->SetCommentText(CheckString(src, "text"));
+
+            float width;
+            if (QueryFloat(src, "width", width)) {
+                comm->SetWidth(width);
+            }
+            float height;
+            if (QueryFloat(src, "height", height)) {
+                comm->SetHeight(height);
+            }
+
+            std::string text;
+            if (QueryString(src, "text", text)) {
+                text = cpputil::StringHelper::UTF8ToGBK(text.c_str());
+                comm->SetCommentText(text);
+            }
+
             bp_node = comm;
         }
             break;
@@ -281,7 +373,12 @@ void ASEImporter::Load(const aseimp::FileLoader& loader)
         case aseimp::NodeClass::SetLocalVar:
         {
             auto set_var = std::make_shared<bp::node::SetLocalVar>();
-            set_var->SetVarName(CheckString(src, "name"));
+
+            std::string name;
+            if (QueryString(src, "name", name)) {
+                set_var->SetVarName(name);
+            }
+
             bp_node = set_var;
         }
             break;
@@ -292,8 +389,19 @@ void ASEImporter::Load(const aseimp::FileLoader& loader)
         {
             auto func = std::make_shared<bp::node::Function>();
 
-            std::string filepath = CheckString(src, "filename");
-            func->SetFilepath(filepath);
+            std::string filename;
+            if (QueryString(src, "filename", filename))
+            {
+                func->SetFilepath(filename);
+
+                auto path = boost::filesystem::absolute(filename + ".asset", dir);
+                if (boost::filesystem::exists(path))
+                {
+                    ASEImporter loader;
+                    loader.LoadAsset(path.string());
+                    func->SetChildren(func, loader.GetNodes());
+                }
+            }
 
             bp_node = func;
         }
@@ -344,7 +452,33 @@ void ASEImporter::Load(const aseimp::FileLoader& loader)
         case aseimp::NodeClass::Smoothstep:
             bp_node = std::make_shared<sg::node::Smoothstep>();
             break;
+        case aseimp::NodeClass::CustomExpression:
+        {
+            auto custom = std::make_shared<sg::node::Custom>();
+
+            std::vector<sg::Node::PinsDesc> inputs, outputs;
+            InputPortASEImpToSG(inputs, src.inputs);
+            OutputPortASEImpToSG(outputs, src.outputs);
+
+            int input_count = CheckInt(src, "input_count");
+            assert(input_count == inputs.size());
+            for (int i = 0; i < input_count; ++i) {
+                inputs[i].name = CheckString(src, "input" + std::to_string(i));
+            }
+
+            custom->ResetPorts(inputs, outputs);
+
+            bp_node = custom;
+        }
+            break;
         case aseimp::NodeClass::StandardSurfaceOutput:
+        {
+            int light_model;
+            if (QueryInt(src, "light_model", light_model)) {
+                assert(light_model == static_cast<int>(aseimp::StandardShaderLightModel::CustomLighting));
+            }
+            bp_node = std::make_shared<sg::node::StandardSurfaceOutput>();
+        }
             break;
         }
 
@@ -410,18 +544,76 @@ void ASEImporter::Load(const aseimp::FileLoader& loader)
 
         auto& from_ports = from->second->GetAllOutput();
         auto& to_ports   = to->second->GetAllInput();
-        if (port_from >= 0 && port_from < from_ports.size() &&
-            port_to >= 0 && port_to < to_ports.size()) {
+        if (port_from >= 0 && port_from < static_cast<int>(from_ports.size()) &&
+            port_to >= 0 && port_to < static_cast<int>(to_ports.size())) {
             bp::make_connecting(from_ports[port_from], to_ports[port_to]);
         } else {
-            if (port_from < 0 || port_from >= from_ports.size()) {
-                assert(from->second->get_type() == rttr::type::get<bp::node::Function>());
-            }
-            if (port_to < 0 || port_to >= to_ports.size()) {
-                assert(to->second->get_type() == rttr::type::get<bp::node::Function>() ||
-                    to->second->get_type() == rttr::type::get<sg::node::Multiply>());
-            }
+            printf("conn fail:%d %d to %d %d\n", node_from, port_from, node_to, port_to);
         }
+    }
+}
+
+int ASEImporter::PortTypeASEImpToBP(aseimp::WirePortDataType type)
+{
+    int ret = bp::PINS_ANY_VAR;
+    switch (type)
+    {
+    case aseimp::WirePortDataType::FLOAT:
+        ret = sg::PINS_VECTOR1;
+        break;
+    case aseimp::WirePortDataType::FLOAT2:
+        ret = sg::PINS_VECTOR2;
+        break;
+    case aseimp::WirePortDataType::FLOAT3:
+        ret = sg::PINS_VECTOR3;
+        break;
+    case aseimp::WirePortDataType::FLOAT4:
+        ret = sg::PINS_VECTOR4;
+        break;
+    case aseimp::WirePortDataType::COLOR:
+        ret = sg::PINS_COLOR;
+        break;
+    case aseimp::WirePortDataType::SAMPLER2D:
+        ret = sg::PINS_TEXTURE2D;
+        break;
+    case aseimp::WirePortDataType::FLOAT3x3:
+        ret = sg::PINS_MATRIX3;
+        break;
+    case aseimp::WirePortDataType::FLOAT4x4:
+        ret = sg::PINS_MATRIX4;
+        break;
+    case aseimp::WirePortDataType::OBJECT:
+        ret = bp::PINS_ANY_VAR;
+        break;
+    default:
+        assert(0);
+    }
+    return ret;
+}
+
+void ASEImporter::InputPortASEImpToSG(std::vector<sg::Node::PinsDesc>& dst,
+                                      const std::vector<aseimp::FileLoader::InputPort>& src)
+{
+    dst.resize(src.size());
+    for (int i = 0, n = src.size(); i < n; ++i)
+    {
+        auto& s = src[i];
+        auto& d = dst[i];
+        d.type = PortTypeASEImpToBP(s.data_type);
+        d.name = s.name;
+    }
+}
+
+void ASEImporter::OutputPortASEImpToSG(std::vector<sg::Node::PinsDesc>& dst,
+                                       const std::vector<aseimp::FileLoader::OutputPort>& src)
+{
+    dst.resize(src.size());
+    for (int i = 0, n = src.size(); i < n; ++i)
+    {
+        auto& s = src[i];
+        auto& d = dst[i];
+        d.type = PortTypeASEImpToBP(s.data_type);
+        d.name = "out";
     }
 }
 
--- src/sgraph/ASEImporter.h
+++ src/sgraph/ASEImporter.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #include <blueprint/typedef.h>
+#include <shadergraph/Node.h>
 
 #include <node0/typedef.h>
+#include <aseimp/FileLoader.h>
 
 #include <string>
 #include <vector>
 #include <map>
-
-namespace aseimp { class FileLoader; }
 
 namespace eone
 {
@@ -26,7 +26,13 @@ public:
     auto& GetNodes() const { return m_nodes; }
 
 private:
-    void Load(const aseimp::FileLoader& loader);
+    void Load(const aseimp::FileLoader& loader, const std::string& dir);
+
+    static int PortTypeASEImpToBP(aseimp::WirePortDataType type);
+    static void InputPortASEImpToSG(std::vector<sg::Node::PinsDesc>& dst,
+        const std::vector<aseimp::FileLoader::InputPort>& src);
+    static void OutputPortASEImpToSG(std::vector<sg::Node::PinsDesc>& dst,
+        const std::vector<aseimp::FileLoader::OutputPort>& src);
 
 private:
     std::vector<n0::SceneNodePtr> m_nodes;
