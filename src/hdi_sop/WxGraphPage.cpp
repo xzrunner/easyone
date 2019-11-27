23ef8fbf30a00e3f1b322c9b81a64746b9b25400
zz <xzrunner@gmail.com>
2019-11-27 08:05:47 +0800

    [CHANGED] move WxGraphPage to sopv

--- src/hdi_sop/WxGraphPage.cpp
+++ src/hdi_sop/WxGraphPage.cpp
@@ -4,35 +4,35 @@
 
 #include "frame/Blackboard.h"
 #include "frame/WxStageSubPanel.h"
-
-#include <ee0/SubjectMgr.h>
-#include <ee0/EditPanelImpl.h>
-#include <ee0/GameObj.h>
-#include <ee0/WxStageCanvas.h>
-#include <ee0/MsgHelper.h>
+//
+//#include <ee0/SubjectMgr.h>
+//#include <ee0/EditPanelImpl.h>
+//#include <ee0/GameObj.h>
+//#include <ee0/WxStageCanvas.h>
+//#include <ee0/MsgHelper.h>
 #include <blueprint/Blueprint.h>
 #include <blueprint/MessageID.h>
-#include <blueprint/NodeHelper.h>
-#include <blueprint/NSCompNode.h>
-#include <blueprint/Node.h>
-#include <blueprint/CompNode.h>
-#include <sopview/Evaluator.h>
+//#include <blueprint/NodeHelper.h>
+//#include <blueprint/NSCompNode.h>
+//#include <blueprint/Node.h>
+//#include <blueprint/CompNode.h>
+//#include <sopview/Evaluator.h>
 #include <sopview/MessageID.h>
-#include <sopview/Node.h>
-#include <sopview/SceneTree.h>
-#include <sopview/Serializer.h>
-#include <sopview/RegistNodes.h>
+//#include <sopview/Node.h>
+//#include <sopview/SceneTree.h>
+//#include <sopview/Serializer.h>
+//#include <sopview/RegistNodes.h>
 #include <sopview/WxToolbarPanel.h>
 #include <sopview/WxStageCanvas.h>
 #include <sopview/WxGeoProperty.h>
-
+//
 #include <node0/SceneNode.h>
 #include <node0/CompComplex.h>
-#include <node2/CompBoundingBox.h>
-#include <node2/AABBSystem.h>
-#include <sx/ResFileHelper.h>
-#include <js/RapidJsonHelper.h>
-#include <ns/CompFactory.h>
+//#include <node2/CompBoundingBox.h>
+//#include <node2/AABBSystem.h>
+//#include <sx/ResFileHelper.h>
+//#include <js/RapidJsonHelper.h>
+//#include <ns/CompFactory.h>
 
 namespace eone
 {
@@ -43,20 +43,8 @@ const std::string WxGraphPage::PAGE_TYPE = "hdi_sop";
 
 WxGraphPage::WxGraphPage(wxWindow* parent, const ee0::GameObj& obj)
     : eone::WxStagePage(parent, obj, 0)
+    , m_sop_page(parent, m_sub_mgr, obj)
 {
-    m_stree = std::make_shared<sopv::SceneTree>();
-    if (!m_obj->HasUniqueComp<bp::CompNode>())
-    {
-        auto& cnode = m_obj->AddUniqueComp<bp::CompNode>();
-        auto node = std::make_shared<sopv::node::Geometry>();
-        node->SetName("obj");
-        cnode.SetNode(node);
-    }
-    m_stree->Add(m_obj);
-#ifdef SOPV_SCENE_TREE_DUMMY_ROOT
-    m_stree->ToNextLevel(m_obj);
-#endif // SOPV_SCENE_TREE_DUMMY_ROOT
-
     static bool inited = false;
     if (!inited) {
         inited = true;
@@ -79,60 +67,6 @@ WxGraphPage::WxGraphPage(wxWindow* parent, const ee0::GameObj& obj)
     RegisterAllMessages();
 
     InitToolbarPanel();
-}
-
-void WxGraphPage::OnNotify(uint32_t msg, const ee0::VariantSet& variants)
-{
-	eone::WxStagePage::OnNotify(msg, variants);
-
-	bool dirty = false;
-	switch (msg)
-	{
-	case ee0::MSG_SCENE_NODE_INSERT:
-		dirty = InsertSceneObj(variants);
-		break;
-	case ee0::MSG_SCENE_NODE_DELETE:
-		dirty = DeleteSceneObj(variants);
-		break;
-	case ee0::MSG_SCENE_NODE_CLEAR:
-        dirty = m_stree->Clear();
-		break;
-
-    case bp::MSG_BP_CONN_INSERT:
-        dirty = AfterInsertNodeConn(variants);
-        break;
-    case bp::MSG_BP_CONN_DELETE:
-        dirty = BeforeDeleteNodeConn(variants);
-        break;
-    case bp::MSG_BP_CONN_REBUILD:
-    {
-        auto eval = m_stree->GetCurrEval();
-        assert(eval);
-        eval->OnRebuildConnection();
-        dirty = true;
-    }
-        break;
-    case bp::MSG_BP_NODE_PROP_CHANGED:
-        dirty = UpdateNodeProp(variants);
-        break;
-
-    case sopv::MSG_CLEAR_NODE_DISPLAY_TAG:
-        m_stree->ClearNodeDisplayTag();
-        dirty = true;
-        break;
-    case sopv::MSG_SCENE_ROOT_TO_NEXT_LEVEL:
-        dirty = ChangeSceneRoot(variants);
-        break;
-    case sopv::MSG_SCENE_ROOT_SEEK_TO_PREV_LEVEL:
-        dirty = PathSeekToPrev(variants);
-        break;
-	}
-
-	if (dirty)
-    {
-		m_sub_mgr->NotifyObservers(ee0::MSG_SET_CANVAS_DIRTY);
-        m_preview_canvas->SetDirty();
-	}
 }
 
 void WxGraphPage::Traverse(std::function<bool(const ee0::GameObj&)> func,
@@ -159,17 +93,17 @@ void WxGraphPage::Traverse(std::function<bool(const ee0::GameObj&)> func,
 
 void WxGraphPage::SetPreviewCanvas(const std::shared_ptr<ee0::WxStageCanvas>& canvas)
 {
-    m_preview_canvas = canvas;
+    m_sop_page.SetPreviewCanvas(canvas);
 
-    if (m_preview_canvas)
+    if (canvas)
     {
         assert(m_toolbar);
         auto prop_view = m_toolbar->GetGeoPropView();
-        prop_view->SetPreviewCanvas(m_preview_canvas);
+        prop_view->SetPreviewCanvas(canvas);
 
-        auto canvas = std::static_pointer_cast<sopv::WxStageCanvas>(m_preview_canvas);
-        canvas->SetPropView(prop_view);
-        canvas->SetGraphStage(this);
+        auto sop_canvas = std::static_pointer_cast<sopv::WxStageCanvas>(canvas);
+        sop_canvas->SetPropView(prop_view);
+        sop_canvas->SetGraphStage(this);
     }
 }
 
@@ -188,131 +122,9 @@ void WxGraphPage::InitToolbarPanel()
     assert(!m_toolbar);
     auto toolbar_panel = Blackboard::Instance()->GetToolbarPanel();
     m_toolbar = static_cast<sopv::WxToolbarPanel*>(toolbar_panel->AddPagePanel([&](wxPanel* parent)->wxPanel* {
-        return new sopv::WxToolbarPanel(toolbar_panel, this, m_stree);
+        return new sopv::WxToolbarPanel(toolbar_panel, this, m_sop_page.GetSceneTree());
     }, wxVERTICAL));
 }
-
-bool WxGraphPage::InsertSceneObj(const ee0::VariantSet& variants)
-{
-    auto var = variants.GetVariant("obj");
-    GD_ASSERT(var.m_type == ee0::VT_PVOID, "no var in vars: obj");
-    const ee0::GameObj* obj = static_cast<const ee0::GameObj*>(var.m_val.pv);
-    GD_ASSERT(obj, "err scene obj");
-
-    return m_stree->Add(*obj);
-}
-
-bool WxGraphPage::DeleteSceneObj(const ee0::VariantSet& variants)
-{
-	auto var = variants.GetVariant("obj");
-	GD_ASSERT(var.m_type == ee0::VT_PVOID, "no var in vars: obj");
-    const ee0::GameObj* obj = static_cast<const ee0::GameObj*>(var.m_val.pv);
-	GD_ASSERT(obj, "err scene obj");
-
-    return m_stree->Remove(*obj);
-}
-
-bool WxGraphPage::AfterInsertNodeConn(const ee0::VariantSet& variants)
-{
-    auto var = variants.GetVariant("conn");
-    GD_ASSERT(var.m_type == ee0::VT_PVOID, "no var in vars: conn");
-    const std::shared_ptr<bp::Connecting>* conn = static_cast<const std::shared_ptr<bp::Connecting>*>(var.m_val.pv);
-    GD_ASSERT(conn, "err conn");
-
-    auto eval = m_stree->GetCurrEval();
-    assert(eval);
-    eval->OnConnected(**conn);
-
-    return true;
-}
-
-bool WxGraphPage::BeforeDeleteNodeConn(const ee0::VariantSet& variants)
-{
-    auto var = variants.GetVariant("conn");
-    GD_ASSERT(var.m_type == ee0::VT_PVOID, "no var in vars: conn");
-    const std::shared_ptr<bp::Connecting>* conn = static_cast<const std::shared_ptr<bp::Connecting>*>(var.m_val.pv);
-    GD_ASSERT(conn, "err conn");
-
-    auto eval = m_stree->GetCurrEval();
-    assert(eval);
-    eval->OnDisconnecting(**conn);
-
-    return true;
-}
-
-bool WxGraphPage::UpdateNodeProp(const ee0::VariantSet& variants)
-{
-    auto var = variants.GetVariant("obj");
-    if (var.m_type == ee0::VT_EMPTY) {
-        return false;
-    }
-
-    GD_ASSERT(var.m_type == ee0::VT_PVOID, "no var in vars: obj");
-    const ee0::GameObj* obj = static_cast<const ee0::GameObj*>(var.m_val.pv);
-    GD_ASSERT(obj, "err scene obj");
-
-    if ((*obj)->HasUniqueComp<bp::CompNode>())
-    {
-        auto& bp_node = (*obj)->GetUniqueComp<bp::CompNode>().GetNode();
-        auto eval = m_stree->GetCurrEval();
-        assert(eval);
-        eval->OnNodePropChanged(bp_node);
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-bool WxGraphPage::ChangeSceneRoot(const ee0::VariantSet& variants)
-{
-    auto var = variants.GetVariant("obj");
-    GD_ASSERT(var.m_type == ee0::VT_PVOID, "no var in vars: obj");
-    const ee0::GameObj* obj = static_cast<const ee0::GameObj*>(var.m_val.pv);
-    GD_ASSERT(obj, "err scene obj");
-
-    if (m_obj == *obj) {
-        return false;
-    } else {
-        m_sub_mgr->NotifyObservers(ee0::MSG_NODE_SELECTION_CLEAR);
-        m_obj = *obj;
-        return true;
-    }
-}
-
-bool WxGraphPage::PathSeekToPrev(const ee0::VariantSet& variants)
-{
-    auto var = variants.GetVariant("depth");
-    GD_ASSERT(var.m_type == ee0::VT_ULONG, "no var in vars: obj");
-    auto depth = var.m_val.ul;
-    if (m_stree->SetDepth(depth)) {
-        m_sub_mgr->NotifyObservers(ee0::MSG_NODE_SELECTION_CLEAR);
-        m_obj = m_stree->GetCurrNode();
-        return true;
-    } else {
-        return false;
-    }
-}
-
-//bool WxGraphPage::UpdateNodes()
-//{
-//    bool dirty = false;
-//    auto& wc = GetImpl().GetCanvas()->GetWidnowContext();
-//    bp::UpdateParams params(wc.wc2, wc.wc3);
-//    Traverse([&](const ee0::GameObj& obj)->bool
-//    {
-//        if (!obj->HasUniqueComp<bp::CompNode>()) {
-//            return true;
-//        }
-//        auto& bp_node = obj->GetUniqueComp<bp::CompNode>().GetNode();
-//        if (bp_node->Update(params)) {
-//            dirty = true;
-//        }
-//        return true;
-//    });
-//    return dirty;
-//}
 
 }
 }
--- src/hdi_sop/WxGraphPage.h
+++ src/hdi_sop/WxGraphPage.h
@@ -6,6 +6,8 @@
 
 #include "frame/WxStagePage.h"
 
+#include <sopview/WxGraphPage.h>
+
 namespace bp { class Node; }
 namespace sopv { class SceneTree; class WxToolbarPanel; }
 
@@ -19,14 +21,12 @@ class WxGraphPage : public eone::WxStagePage
 public:
 	WxGraphPage(wxWindow* parent, const ee0::GameObj& obj);
 
-    virtual void OnNotify(uint32_t msg, const ee0::VariantSet& variants) override;
-
     virtual void Traverse(std::function<bool(const ee0::GameObj&)> func,
         const ee0::VariantSet& variants = ee0::VariantSet(), bool inverse = false) const override;
 
     virtual int GetPageType() const { return 0; }
 
-    auto GetSceneTree() const { return m_stree; }
+    auto GetSceneTree() const { return m_sop_page.GetSceneTree(); }
 
     void SetPreviewCanvas(const std::shared_ptr<ee0::WxStageCanvas>& canvas);
 
@@ -42,24 +42,10 @@ protected:
 private:
     void InitToolbarPanel();
 
-    bool InsertSceneObj(const ee0::VariantSet& variants);
-    bool DeleteSceneObj(const ee0::VariantSet& variants);
-
-    bool AfterInsertNodeConn(const ee0::VariantSet& variants);
-    bool BeforeDeleteNodeConn(const ee0::VariantSet& variants);
-    bool UpdateNodeProp(const ee0::VariantSet& variants);
-
-    bool ChangeSceneRoot(const ee0::VariantSet& variants);
-    bool PathSeekToPrev(const ee0::VariantSet& variants);
-
-//    bool UpdateNodes();
-
 private:
+    sopv::WxGraphPage m_sop_page;
+
     sopv::WxToolbarPanel* m_toolbar = nullptr;
-
-    std::shared_ptr<sopv::SceneTree> m_stree = nullptr;
-
-    std::shared_ptr<ee0::WxStageCanvas> m_preview_canvas = nullptr;
 
 }; // WxGraphPage
 
